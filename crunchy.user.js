a99730b3e4a8f4b2517c3c88dcf0788b92e559a7
Multarix <darien2105@gmail.com>
2026-08-19 16:31:56 +1000

    Clean up the code a smidgen

--- crunchy.user.js
+++ crunchy.user.js
@@ -7,28 +7,21 @@
 // @match        https://www.crunchyroll.com/*
 // @run-at       document-start
 // @grant        none
-// @updateURL    https://git.multarix.com/Multarix/Crunchyroll-Chunk-Fix/raw/branch/master/crunchy.user.js
-// @downloadURL  https://github.com/Multarix/Crunchyroll-Chunk-Fix/raw/branch/master/crunchy.user.js
+// @updateURL    https://github.com/Multarix/Crunchyroll-Chunk-Fix/raw/refs/heads/master/crunchy.user.js
+// @downloadURL  https://github.com/Multarix/Crunchyroll-Chunk-Fix/raw/refs/heads/master/crunchy.user.js
 // ==/UserScript==
 
 (() => {
-    const ORIGINAL =
-        "https://www.crunchyroll.com/build/_next/static/chunks/8cf7a42c.8e367a181ce67b7f.js";
+	const ORIGINAL = "https://www.crunchyroll.com/build/_next/static/chunks/8cf7a42c.8e367a181ce67b7f.js";
+	const REPLACEMENT = "https://multarix.com/static/crunchy/8cf7a42c.8e367a181ce67b7f.js";
+	
+	const originalAppendChild = Node.prototype.appendChild;
+	Node.prototype.appendChild = function (node) {
+		if(node instanceof HTMLScriptElement && new URL(node.src, location.href).href === ORIGINAL){
+			console.log("[CR Fix] Replacing 8cf7a42c chunk");
+			node.src = REPLACEMENT;
+		}
 
-    const REPLACEMENT =
-        "https://multarix.com/static/crunchy/8cf7a42c.8e367a181ce67b7f.js";
-
-    const originalAppendChild = Node.prototype.appendChild;
-
-    Node.prototype.appendChild = function (node) {
-        if (
-            node instanceof HTMLScriptElement &&
-            new URL(node.src, location.href).href === ORIGINAL
-        ) {
-            console.log("[CR Fix] Replacing 8cf7a42c chunk");
-            node.src = REPLACEMENT;
-        }
-
-        return originalAppendChild.call(this, node);
-    };
+		return originalAppendChild.call(this, node);
+	};
 })();
